14ba00b545c32aad82a09442291e4a7bf9397cc5
nabil <nabil@ll.mit.edu>
2019-08-15 10:56:37 -0400

    clean up sgx

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,14 @@
 
 // #![warn(missing_docs)]
 
-#![cfg_attr(not(target_env = "sgx"), no_std)]
+#![cfg_attr(target_env = "sgx", no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 
-#[cfg(not(target_env = "sgx"))]
+#[cfg(target_env = "sgx")]
 #[macro_use]
 extern crate sgx_tstd as std;
+
+use std::prelude::v1::*;
 
 extern crate endian_type;
 extern crate nibble_vec;
